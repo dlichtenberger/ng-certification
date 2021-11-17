fa848b09ac1cbb3e119f395eaf2fd2560bd98911
dlichtenberger <daniel.lichtenberger@gmail.com>
2021-11-17 15:54:35 +0100

    Initial weather list implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 import { LocalStorageService } from './local-storage.service';
+import { WeatherReport, WeatherService } from './weather.service';
 
 @Component({
   selector: 'my-app',
@@ -10,25 +12,65 @@ export class AppComponent {
   name = 'Angular';
 
   zipCodeInput = '';
-  zipCodes: string[] = [];
+  reports: WrappedReport[] = [];
 
-  constructor(private localStorage: LocalStorageService) {
-    this.zipCodes = localStorage.load(STORAGE_ZIP_CODES) || [];
+  constructor(
+    private localStorage: LocalStorageService,
+    private weatherService: WeatherService
+  ) {
+    let zipCodes: string[] = localStorage.load(STORAGE_ZIP_CODES) || [];
+    for (let zipCode of zipCodes) {
+      this.requestWeatherReport(zipCode);
+    }
   }
 
-  addZipCode() {
+  addZipCodeInput() {
     if (this.zipCodeInput) {
       let trimmed = this.zipCodeInput.trim();
       if (trimmed) {
-        this.zipCodes.push(trimmed);
-        this.syncZipCodes();
+        this.add(trimmed);
       }
     }
   }
 
-  private syncZipCodes() {
-    this.localStorage.save(STORAGE_ZIP_CODES, this.zipCodes);
+  deleteReport(row: WrappedReport) {
+    this.reports.splice(this.reports.indexOf(row), 1);
+    this.syncZipCodes();
   }
+
+  private add(zipCode: string) {
+    if (this.reports.map((report) => report.zipCode).indexOf(zipCode) !== -1) {
+      alert(`ZIP code already shown: ${zipCode}`);
+      return;
+    }
+    this.requestWeatherReport(zipCode);
+  }
+
+  private addReport(report: WrappedReport) {
+    this.reports.push(report);
+    this.reports.sort((a, b) => a.zipCode.localeCompare(b.zipCode));
+    this.syncZipCodes();
+  }
+
+  private requestWeatherReport(zipCode: string) {
+    this.weatherService.loadByZip(zipCode).subscribe({
+      next: (data) => this.addReport({ zipCode: zipCode, report: data }),
+      error: (_error) =>
+        alert(`Failed to retrieve weather information for ZIP code ${zipCode}`),
+    });
+  }
+
+  private syncZipCodes() {
+    this.localStorage.save(
+      STORAGE_ZIP_CODES,
+      this.reports.map((report) => report.zipCode)
+    );
+  }
+}
+
+interface WrappedReport {
+  zipCode: string;
+  report: WeatherReport;
 }
 
 const STORAGE_ZIP_CODES = 'forecastZipCodes';
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -10,13 +12,59 @@ export class WeatherService {
   constructor(private http: HttpClient) {}
 
   loadByZip(zipCode: string): Observable<WeatherReport> {
-    return this.http.get<WeatherReport>(
-      `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${this.APP_ID}`
+    return this.getReportForZip(zipCode).pipe(
+      map((response) => ({
+        zipCode: zipCode,
+        name: response.name,
+        conditions: response.weather[0].main,
+        icon: CONDITION_ICONS[response.weather[0].main] || WeatherIcon.SUN,
+        temperature: {
+          minimum: response.main.temp_min,
+          maximum: response.main.temp_max,
+          current: response.main.temp,
+        },
+      }))
+    );
+  }
+
+  private getReportForZip(zipCode: string): Observable<WeatherResponse> {
+    return this.http.get<WeatherResponse>(
+      `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${this.APP_ID}&units=imperial`
     );
   }
 }
 
 export interface WeatherReport {
+  zipCode: string;
+  name: string;
+  conditions: string;
+  icon: WeatherIcon;
+  temperature: {
+    minimum: number;
+    maximum: number;
+    current: number;
+  };
+}
+
+export enum WeatherIcon {
+  SUN = 'https://www.angulartraining.com/images/weather/sun.png',
+  SNOW = 'https://www.angulartraining.com/images/weather/snow.png',
+  RAIN = 'https://www.angulartraining.com/images/weather/rain.png',
+  CLOUDS = 'https://www.angulartraining.com/images/weather/clouds.png',
+}
+
+// based on https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
+const CONDITION_ICONS = {
+  Clear: WeatherIcon.SUN,
+  Snow: WeatherIcon.SNOW,
+  Rain: WeatherIcon.RAIN,
+  Drizzle: WeatherIcon.RAIN,
+  Thunderstorm: WeatherIcon.RAIN,
+  Clouds: WeatherIcon.CLOUDS,
+  Mist: WeatherIcon.CLOUDS,
+};
+
+interface WeatherResponse {
   coord: {
     lon: number;
     lat: number;
